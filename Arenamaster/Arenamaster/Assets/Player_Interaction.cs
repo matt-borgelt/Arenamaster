565fd389b3f31d6bfbf41c14c1f40629af879311
mkborgelt13 <mkborgelt13@gmail.com>
2015-05-31 00:08:15 -0500

    Context menu
    right click objects to show a menu with options, a few other things

--- Arenamaster/Arenamaster/Assets/Player_Interaction.cs
+++ Arenamaster/Arenamaster/Assets/Player_Interaction.cs
@@ -10,37 +10,84 @@ public class Player_Interaction : MonoBehaviour {
 	public Ray sightLine;
 
 	public Text text;
+	public Text console;
+	public GameObject selected;
 	public bool menu;
 	Vector3 position;
+	public Rect window;
+	public GUISkin mySkin;
+
+	public RaycastHit hit;
 
 	void Start () {
-
+		menu = false;
+		selected = new GameObject();
 	}
 
 	void Update () {
 
 		Ray ray = cameraObject.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
+		//RaycastHit hit;
 		if (Physics.Raycast(ray, out hit)){
 			// the object identified by hit.transform was clicked
 			text.text = ( hit.transform.ToString());
 		}
 
 		if (Input.GetMouseButtonDown (1)) {
+
+			selected = GameObject.Find(hit.collider.gameObject.name);   //hit.transform.gameObject
 			position = Input.mousePosition;
 			menu=true;
 		}
 		if (Input.GetMouseButtonUp (1)) {
 			menu=false;
+			selected = null;
 		}
 	}
 
 	void OnGUI(){
-
+		GUI.skin = mySkin;
 		if (menu){
-			string[] header = text.text.ToString().Split(new string[] {" "}, System.StringSplitOptions.RemoveEmptyEntries);
-			GUI.Box(new Rect(position.x,Screen.height-position.y, 120, 120), header[0]);
+			string[] header = selected.ToString().Split(new string[] {"("}, System.StringSplitOptions.RemoveEmptyEntries);
 
+
+			if(selected.gameObject.name=="Tim tom"){
+				window = GUI.Window(0, new Rect(position.x,Screen.height-position.y, 200, 120), ContextMenuNPC, header[0] );
+			}
+
+			else if(selected.gameObject.name=="Player"){
+				window = GUI.Window(0, new Rect(position.x,Screen.height-position.y, 200, 120), ContextMenuNPC, header[0] );
+			}
+			if(selected.gameObject.name=="Terrain"){
+				window = GUI.Window(0, new Rect(position.x,Screen.height-position.y, 200, 120), ContextMenuGeneric, header[0] );
+			}
 		}
+	}
+
+	void ContextMenuGeneric(int windowId){
+		if (GUI.Button (new Rect (10, 20, 100, 20), "Examine")) {
+			if(selected.gameObject.name == "Terrain"){
+				console.text="That is the ground.";
+			}
+		}
+	}
+
+	void ContextMenuNPC(int windowId){
+		if (GUI.Button (new Rect (10, 20, 100, 20), "Examine")) {
+			if(selected.gameObject.name == "Player"){
+				console.text="That is you.";
+			}
+			if(selected.transform.gameObject.name=="Tim tom"){
+				console.text="Your opponent, the enemy.";
+			}
+		}
+
+		if(GUI.Button (new Rect (10, 40+1, 100, 20), "Attack")){
+			if(selected.gameObject.name=="Tim tom"){
+				console.text="You attack the NPC.";
+			}
+		}
+		GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+
 	}
 }
